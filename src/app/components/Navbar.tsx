1251e0bfcef37b8401ac1ec4d09feaf83338e87f
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2024-06-24 16:56:03 +0800

    hero section

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+// App.tsx
+
+import React, { useEffect, useState } from "react";
+import styles from "./Navbar.module.scss";
+
+const useScroll = () => {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    lastX: 0,
+    lastY: 0,
+  });
+
+  const handleScroll = () => {
+    setData((last) => ({
+      x: window.scrollX,
+      y: window.scrollY,
+      lastX: last.x,
+      lastY: last.y,
+    }));
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return data;
+};
+
+const Navbar: React.FC = () => {
+  const [navClassList, setNavClassList] = useState<string[]>([]); // Explicitly define initial state as string[]
+
+  const scroll = useScroll();
+
+  // update classList of nav on scroll
+  useEffect(() => {
+    const _classList: string[] = [];
+
+    if (scroll.y > 150 && scroll.y - scroll.lastY > 0) {
+      _classList.push(styles["nav-bar--hidden"]);
+    }
+
+    setNavClassList(_classList);
+  }, [scroll.y, scroll.lastY]);
+
+  return (
+    <nav className={`${styles.nav} ${navClassList.join(" ")}`}>
+      <div className={`${styles.link} ${styles.active}`}>Home</div>
+      <div className={styles.link}>About</div>
+      <div className={styles.link}>Works</div>
+      <div className={styles.link}>Contact</div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,51 @@
 import Image from "next/image";
+import Spline from "@splinetool/react-spline/next";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.red}>HELLO THIS IS RED</h1>
-      hello
-    </main>
+    <>
+      <div className={styles.hero}>
+        <div className={styles.overlay}>
+          <div className={styles.heroSection}>
+            <div className={styles.heroImage}>
+              <Image
+                src="/assets/shapes/star.svg"
+                width={44}
+                height={44}
+                alt="Star"
+              />
+
+              <Image
+                src="/assets/images/dhea.png"
+                width={118}
+                height={83}
+                alt="Dhea"
+              />
+
+              <Image
+                src="/assets/shapes/star.svg"
+                width={44}
+                height={44}
+                alt="Star"
+              />
+            </div>
+            <h1 className={styles.heroText}>
+              Crafting seamless, functional, beautiful web experiences
+            </h1>
+            <p className={styles.scroll}>Scroll down</p>
+          </div>
+        </div>
+        <Spline scene="https://prod.spline.design/StMq9TNTN2eHvGB8/scene.splinecode" />
+      </div>
+      <main className={styles.main}>
+        <h1 className={styles.red}>HELLO THIS IS RED</h1>
+        hello
+      </main>
+      <main className={styles.main}>
+        <h1 className={styles.red}>HELLO THIS IS RED</h1>
+        hello
+      </main>
+    </>
   );
 }
