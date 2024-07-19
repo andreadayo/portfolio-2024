c2f65921a843e5d63aba2e932181e6357a42ae5a
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2024-07-19 08:41:58 +0800

    improve transition

--- src/app/components/animation/Stairs.tsx
+++ src/app/components/animation/Stairs.tsx
@@ -2,44 +2,14 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import { opacity, expand } from "./anim";
 import styles from "./Stairs.module.scss";
 
-export const expand = {
-  initial: {
-    top: 0,
-  },
-  enter: (i: number) => ({
-    top: "100vh",
-    transition: {
-      duration: 0.4,
-      delay: 0.05 * i,
-      ease: [0.215, 0.61, 0.355, 1],
-    },
-    transitionEnd: { height: "0", top: "0" },
-  }),
-  exit: (i: number) => ({
-    height: "100vh",
-    transition: {
-      duration: 0.4,
-      delay: 0.05 * i,
-      ease: [0.215, 0.61, 0.355, 1],
-    },
-  }),
-};
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
-export const opacity = {
-  initial: {
-    opacity: 0.5,
-  },
-  enter: {
-    opacity: 0,
-  },
-  exit: {
-    opacity: 0.5,
-  },
-};
-
-export default function Stairs({ children }: { children: React.ReactNode }) {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   const anim = (variants: any, custom: number | null = null) => {
     return {
       initial: "initial",
@@ -56,11 +26,13 @@ export default function Stairs({ children }: { children: React.ReactNode }) {
     <div className={styles.stairs}>
       <motion.div {...anim(opacity)} className={styles.transitionBackground} />
       <div className={styles.transitionContainer}>
-        {[...Array(nbOfColumns)].map((_, i) => {
-          return <motion.div key={i} {...anim(expand, nbOfColumns - i)} />;
-        })}
+        {[...Array(nbOfColumns)].map((_, i) => (
+          <motion.div key={i} {...anim(expand, nbOfColumns - i)} />
+        ))}
       </div>
       {children}
     </div>
   );
-}
+};
+
+export default Layout;
